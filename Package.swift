7ad52ca668c28218905c35e532d7db66d82db637
ILBuzyrev <ILBuzyrev@sberbank.ru>
2023-07-05 16:06:56 +0300

    Change Package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "JazzSDK",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         .library(
             name: "JazzSDK",
